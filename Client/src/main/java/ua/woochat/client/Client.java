1c8c6f4968ad7fd89ba19e5295d666fb9d8c8b1c
rayvoid <rayvoid@ukr.net>
2019-02-09 12:02:05 +0200

    Hello World

--- Client/src/main/java/ua/woochat/client/Client.java
+++
@@ -1,7 +0,0 @@
-package ua.woochat.client;
-
-public class Client {
-    public void sayHello() {
-        System.out.println("Hello from Client");
-    }
-}
---
+++ Client/src/main/java/ua/woochat/listeners/Hello.java
@@ -0,0 +1,4 @@
+package ua.woochat.listeners;
+
+public class Hello {
+}
