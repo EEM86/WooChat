e2942181d993d58fbbbe704e9051ca29bda0a1d5
EEM86 <eevgeniy@gmail.com>
2019-02-18 14:06:43 +0200

    deleted wrong classes

--- Client/src/main/java/ua/woochat/client/MainClient.java
+++
@@ -1,9 +0,0 @@
-package ua.woochat.client;
-
-import java.io.IOException;
-
-public class MainClient {
-    public static void main(String[] args) throws IOException {
-        new TestClient();
-    }
-}
